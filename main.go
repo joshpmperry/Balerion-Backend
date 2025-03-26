7cd98a4b9dc6c1ad314a131032de0b0eab8b4b28
Josh Perry <joshpmperry@gmail.com>
2025-03-26 23:59:07 +0700

    Revised version to address a bug when a number is over a million

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
-	"os"
-	"strings"
 
 	"github.com/shopspring/decimal"
 )
+
+// error handling for negative numbers (Not sure what to do when there is a negative number)
+var ErrNegativeNumber = errors.New("negative numbers are not supported")
 
 var thaiNumbers = []string{"ศูนย์", "หนึ่ง", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า"}
 var thaiUnits = []string{"", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน"}
@@ -16,32 +17,63 @@ func convertInteger(num int64) string {
 	if num == 0 {
 		return thaiNumbers[0]
 	}
+	// Split number into millions and less than millions (remainder)
+	millions := num / 1000000
+	remainder := num % 1000000
+
 	result := ""
-	digits := []int{}
-	for num > 0 {
-		digits = append([]int{int(num % 10)}, digits...)
-		num /= 10
+	if millions > 0 {
+		result += lessThanMillionConvertion(millions) + "ล้าน"
+	}
+	if remainder > 0 {
+		result += lessThanMillionConvertion(remainder)
 	}
 
-	n := len(digits)
-	for i, digit := range digits {
+	return result
+}
+
+// Handles numbers < 1 million
+func lessThanMillionConvertion(num int64) string {
+	if num == 0 {
+		return ""
+	}
+
+	digits := []int{}
+	n := num
+	for n > 0 {
+		digits = append([]int{int(n % 10)}, digits...)
+		n /= 10
+	}
+
+	result := ""
+	for i := 0; i < len(digits); i++ {
+		digit := digits[i]
+		position := len(digits) - i - 1
+
 		if digit == 0 {
 			continue
 		}
-		if digit == 1 && i == n-2 && n > 1 {
-			result += "สิบ"
-		} else if digit == 2 && i == n-2 && n > 1 {
-			result += "ยี่สิบ"
-		} else if digit == 1 && i == n-1 && n > 1 && digits[i-1] != 0 {
+		// Special case for สิบ and ยี่สิบ
+		if position == 1 {
+			if digit == 1 {
+				result += "สิบ"
+				continue
+			}
+			if digit == 2 {
+				result += "ยี่สิบ"
+				continue
+			}
+		}
+
+		// Special case for เอ็ด
+		if position == 0 && digit == 1 && len(digits) > 1 && digits[len(digits)-2] != 0 {
 			result += "เอ็ด"
 		} else {
 			result += thaiNumbers[digit]
 		}
 
-		// Check for out of bounds before accessing thaiUnits
-		unitIndex := n - i - 1
-		if unitIndex >= 0 && unitIndex < len(thaiUnits) && i < n-1 {
-			result += thaiUnits[unitIndex]
+		if position > 0 {
+			result += thaiUnits[position]
 		}
 	}
 
@@ -58,7 +90,11 @@ func convertFraction(num int64) string {
 	return convertInteger(num) + "สตางค์"
 }
 
-func DecimalToThaiBaht(value decimal.Decimal) string {
+func DecimalToThaiBaht(value decimal.Decimal) (string, error) {
+	if value.IsNegative() {
+		return "", ErrNegativeNumber
+	}
+
 	intPart := value.IntPart()
 	fractionalPart := value.Sub(decimal.NewFromInt(intPart)).Mul(decimal.NewFromInt(100)).IntPart()
 
@@ -68,53 +104,68 @@ func DecimalToThaiBaht(value decimal.Decimal) string {
 	} else {
 		result += convertFraction(fractionalPart)
 	}
-	return result
-}
-
-// Used for input files when passing through executable
-func processInputFile(filename string) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return fmt.Errorf("error opening file: %w", err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		line = strings.TrimSpace(line)
-		if line == "" {
-			continue
-		}
-
-		num, err := decimal.NewFromString(line)
-		if err != nil {
-			fmt.Printf("Error converting '%s': %v\n", line, err)
-			continue
-		}
-
-		thaiText := DecimalToThaiBaht(num)
-		fmt.Println(thaiText)
-	}
-
-	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("error reading file: %w", err)
-	}
-
-	return nil
+	return result, nil
 }
 
 func main() {
-	// Input file number.txt -> change the numbers to your liking :)
-	if len(os.Args) > 1 {
-		filename := os.Args[1]
-		err := processInputFile(filename)
+	inputs := []decimal.Decimal{
+		decimal.NewFromFloat(999999999.99), // Change this number to whatever you'd like
+	}
+
+	for _, input := range inputs {
+		result, err := DecimalToThaiBaht(input)
 		if err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
+			fmt.Printf("Error: %v\n", err)
+			continue
 		}
-	} else {
-		fmt.Println("Usage: go run main.go <input_file>")
-		os.Exit(1)
+		fmt.Printf("%v: %s\n", input, result)
 	}
 }
+
+// Used for input files when passing through executable
+// func processInputFile(filename string) error {
+// 	file, err := os.Open(filename)
+// 	if err != nil {
+// 		return fmt.Errorf("error opening file: %w", err)
+// 	}
+// 	defer file.Close()
+
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		line := scanner.Text()
+// 		line = strings.TrimSpace(line)
+// 		if line == "" {
+// 			continue
+// 		}
+
+// 		num, err := decimal.NewFromString(line)
+// 		if err != nil {
+// 			fmt.Printf("Error converting '%s': %v\n", line, err)
+// 			continue
+// 		}
+
+// 		thaiText := DecimalToThaiBaht(num)
+// 		fmt.Println(thaiText)
+// 	}
+
+// 	if err := scanner.Err(); err != nil {
+// 		return fmt.Errorf("error reading file: %w", err)
+// 	}
+
+// 	return nil
+// }
+
+// func main() {
+// 	// Input file number.txt -> change the numbers to your liking :)
+// 	if len(os.Args) > 1 {
+// 		filename := os.Args[1]
+// 		err := processInputFile(filename)
+// 		if err != nil {
+// 			fmt.Println("Error:", err)
+// 			os.Exit(1)
+// 		}
+// 	} else {
+// 		fmt.Println("Usage: go run main.go <input_file>")
+// 		os.Exit(1)
+// 	}
+// }
